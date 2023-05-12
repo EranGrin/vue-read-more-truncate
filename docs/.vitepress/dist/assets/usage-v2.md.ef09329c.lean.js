90249592430fdc0150a64ba14e9090dfa152aea3
Eran Grinberg <soniceran@gmail.com>
2023-05-13 00:40:10 +0200

    FEATURE | Add the docs as html

---
+++ docs/.vitepress/dist/assets/usage-v2.md.ef09329c.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,O as l}from"./chunks/framework.d7e37bdc.js";const m=JSON.parse('{"title":"Usage v2","description":"","frontmatter":{},"headers":[],"relativePath":"usage-v2.md","filePath":"usage-v2.md"}'),e={name:"usage-v2.md"},t=l("",4),o=[t];function p(c,r,D,F,y,i){return n(),a("div",null,o)}const d=s(e,[["render",p]]);export{m as __pageData,d as default};
